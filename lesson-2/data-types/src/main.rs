387206ca1f9cb449a1c262bd516ff51e07b6cc18
wyMinLwin <waiyanminlwin421@gmail.com>
2024-11-25 22:48:27 +0700

    array

--- lesson-2/data-types/src/main.rs
+++ lesson-2/data-types/src/main.rs
@@ -17,4 +17,14 @@ fn main() {
     //spread tuple
     let (a, b, c) = tup;
     println!("{} {} {}", a, b, c);
+
+    //array
+    let array = [1, 2, 3, 4, 5];
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+    let a: [i32: 5] = [1, 2, 3, 4, 5];
+
+
+    let b = [3; 5];
+    // b = [3, 3, 3, 3, 3]
 }
