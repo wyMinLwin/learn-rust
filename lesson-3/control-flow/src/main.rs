9bf983736957e820c94229d71daf0ad8b5672bae
wyMinLwin <waiyanminlwin421@gmail.com>
2024-11-28 23:33:50 +0700

    :memo: Loops

--- lesson-3/control-flow/src/main.rs
+++ lesson-3/control-flow/src/main.rs
@@ -11,4 +11,33 @@ fn main() {
     let condition = true;
     let age = if condition { 21 } else { 20 };
     println!("Age is {}", age);
+
+
+    //loops
+    let mut counter = 0;
+    let result = loop {
+        counter += 1;
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+    println!("Result is {}", result);
+
+
+    let mut count = 0;
+    'counting_up: loop {
+        println!("Counting up {}", count);
+        let mut remaining = 10;
+        loop {
+            println!("Remaining {}", remaining);
+            if remaining == 9 {
+                break;
+            }
+            if count == 2 {
+                break 'counting_up;
+            }
+            remaining -= 1;
+        }
+        count += 1;
+    }
 }
