65d9f26a5888b74ca7c9b957ccbb93f63f4e1ca5
wyMinLwin <waiyanminlwin421@gmail.com>
2025-01-28 23:42:22 +0700

    :memo: Lesson 10: Traits

---
+++ lesson-10/traits/src/main.rs
@@ -0,0 +1,24 @@
+use aggregator::{Summary, Tweet, NewsArticle};
+
+fn main() {
+    let tweet = Tweet {
+        username: String::from("armin_arlert"),
+        content: String::from(
+            "Sasageyo! Sasageyo! Shinzou wo sasageyo! Subete no gisei wa ima kono toki no tame ni!",
+        ),
+        reply: false,
+        retweet: false,
+    };
+
+    let news_article = NewsArticle {
+        headline: String::from("The Attack Titan"),
+        location: String::from("Paradis Island"),
+        author: String::from("Hajime Isayama"),
+        content: String::from(
+            "The Attack Titan is a manga series written and illustrated by Hajime Isayama.",
+        ),
+    };
+
+    println!("1 new article: {}", news_article.summarize());
+    println!("1 new tweet: {}", tweet.summarize());
+}
