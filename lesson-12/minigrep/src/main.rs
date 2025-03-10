e1d49167cd21cf4f3b3e918f2431435e8cdb97d8
wyMinLwin <waiyanminlwin421@gmail.com>
2025-03-11 02:33:54 +0700

    :memo: Lesson 12 - Split Config

--- lesson-12/minigrep/src/main.rs
+++ lesson-12/minigrep/src/main.rs
@@ -1,15 +1,21 @@
 use std::env;
 use std::fs;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args);
-    // println!("Searching for {}", config.query);
-    // println!("In file {}", config.file_path);
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
-    let contents = fs::read_to_string(config.file_path).expect("Something went wrong reading the file");
-    println!("With text:\n{}", contents);
+    run(config);
+}
+
+fn run(config:Config) {
+    let contents = fs::read_to_string(config.file_path).expect("Should be a valid file path");
+    println!("With text:\n{contents}");
 }
 
 struct Config {
@@ -18,9 +24,12 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("Not enough arguments");
+        }
         let query = args[1].clone();
         let file_path = args[2].clone();
-        Config { query, file_path }
+        Ok(Config { query, file_path })
     }
 }
