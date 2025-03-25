20a364da4a8a038ef75857953f65d7427f4130de
wyMinLwin <waiyanminlwin421@gmail.com>
2025-03-26 02:30:47 +0700

    :memo: Lesson 13 closures started

---
+++ lesson-13/annotations/src/main.rs
@@ -0,0 +1,26 @@
+fn main() {
+
+    //closure
+    let add_one = | x | {x +1};
+    let x = 5;
+    println!("{} + 1 = {}", x, add_one(x));
+
+    //closure which capture immutable variable
+    let list = vec![1,2,3];
+    
+    //before define closure
+    println!("Before define closure: {list:?}");
+
+    let borrow_list = || println!("Closure: {list:?}");
+
+    println!("before call closure: {list:?}");
+    borrow_list();
+    println!("after call closure: {list:?}");
+
+    //closure which capture mutable variable
+    let mut mut_list = vec![4,5,6];
+    println!("Before define closure: {:?}", mut_list);
+    let mut mut_borrow_list = || mut_list.push(21);
+    mut_borrow_list();
+    println!("After define closure: {mut_list:?}");
+}
